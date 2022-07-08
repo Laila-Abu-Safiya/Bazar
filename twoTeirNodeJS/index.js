5f1b8ba4f92d7714db046e6776a7d025d982fe69
laila <labus1249@gmail.com>
2022-07-08 18:04:14 +0300

    full project

---
+++ twoTeirNodeJS/index.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+var Book_Sold = []
+var catalog = []
+var parser = parse({ columns: true }, function(err, Catalog) {
+    catalog = Catalog;
+});
+
+fs.createReadStream('catalog.csv').pipe(parser);
+var parser_Order = parse({ columns: true }, function(err, Order) {
+    Book_Sold = Order;
+});
+
+fs.createReadStream('order.csv').pipe(parser_Order);
+
+const express = require('express');
+const { text } = require('express');
+const e = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello world!!!');
+});
+app.get('/CATALOG_WEBSERVICE_IP', (req, res) => {
+    let result = catalog.map(o => ({ id: parseInt(o.id), price: parseInt(o.price), tittle: o.tittle, quantity: parseInt(o.quantity), topic: o.topic }));
+    res.send(result);
+});
+app.get('/CATALOG_WEBSERVICE_IP/search/:itemName', (req, res) => {
+    let Book = catalog.filter((c => c.topic === req.params.itemName));
+
+    if (!Book) res.status(404).send('The Books is not found!');
+    else {
+        let result = Book.map(o => ({
+            id: parseInt(o.id),
+            tittle: o.tittle
+        }));
+        res.send(result);
+    }
+});
+
+app.get('/CATALOG_WEBSERVICE_IP/info/:itemNUM', (req, res) => {
+    let Book = catalog.filter((c => c.id === req.params.itemNUM));
+    if (!Book) res.status(404).send('The Book is not found!');
+    let result = Book.map(o => ({ tittle: o.tittle, quantity: parseInt(o.quantity), price: parseInt(o.price) }));
+    res.send(result[0]);
+});
+app.get('/CATALOG_WEBSERVICE_IP/purchase/:itemNUM', (req, res) => {
+    let Book = catalog.filter((c => c.id === req.params.itemNUM));
+    if (!Book) res.status(404).send('The Book is not found!');
+    else {
+        for (var i in catalog) {
+            if (catalog[i].id === req.params.itemNUM) {
+                if (catalog[i].quantity > 0) {
+                    catalog[i].quantity = `${(parseInt(catalog[i].quantity) - 1)}`;
+                    Book_Sold[i].quantity = `${(parseInt(Book_Sold[i].quantity) + 1)}`;
+                    const csvWriter = createCsvWriter({
+                        path: 'catalog.csv',
+                        header: [
+                            { id: 'id', title: 'id' },
+                            { id: 'price', title: 'price' },
+                            { id: 'tittle', title: 'tittle' },
+                            { id: 'quantity', title: 'quantity' },
+                            { id: 'topic', title: 'topic' }
+                        ]
+                    });
+                    csvWriter
+                        .writeRecords(catalog)
+                        .then(() => console.log(''));
+                    const csvWriterOrder = createCsvWriter({
+                        path: 'order.csv',
+                        header: [
+                            { id: 'id', title: 'id' },
+                            { id: 'tittle', title: 'tittle' },
+                            { id: 'quantity', title: 'quantity' }
+                        ]
+                    });
+                    csvWriterOrder
+                        .writeRecords(Book_Sold)
+                        .then(() => console.log(``));
+                    res.send(`Bought book ${catalog[i].tittle}`);
+                }
+            }
+        }
+
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(` Listeningon port $ { port }...`))
